a538a1ef0ba13a1a9035adf8641530eb576262d6
JonasPuettmann <jonaspuettmann2009@gmail.com>
2025-07-31 06:15:50 +0200

    Update player.gd

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var jump_height = 0
 
 @onready var anim : AnimatedSprite2D = $AnimatedSprite2D
 
@@ -21,8 +22,12 @@ func _physics_process(delta):
 		anim.flip_h = true
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if not Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_height
+	
+	while Input.is_action_pressed("ui_accept"):
+		jump_height += 1
+		await get_tree().create_timer(0.001).timeout
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
